be82c828921d20c4bccc26fdd9b9315d68d4ab4e
chaitanyawargand <chaitanyawargand@gmail.com>
2025-07-10 18:38:14 +0530

    Add files via upload

---
+++ backened/controllers/groupController.js
@@ -0,0 +1,83 @@
+const Group = require('../models/groupModel');
+const User=require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.createGroup =catchAsync(async (req, res, next) => {
+    const groupData = {
+        name: req.body.name,
+        description: req.body.description,
+        members: [req.user.id],
+        createdBy: req.user.id
+    };
+    const newGroup = await Group.create(groupData);
+    res.status(201).json({
+        status: 'success',
+        data: {
+        group: newGroup
+        }
+    });
+})
+exports.addMember= catchAsync(async(req,res,next)=>{
+    const groupId= req.params.id;
+    const friendId=req.body.friendId;
+     if(!friendId) {return next(new AppError('Please provide a friend ID', 400));}
+    const group = await Group.findById(groupId);
+   if (!group) return next(new AppError('Group not found', 404));
+   if(!group.createdBy.equals(req.user.id)) {
+    return next(new AppError('Only the group creator can add members', 403));} 
+    const currentUser= await User.findById(req.user.id).select('friends');
+  const isFriend= currentUser.friends.some(friend=>friend.toString()===friendId);
+  if (!isFriend) {return next(new AppError('This user is not your friend', 403));}
+  const alreadyMember = group.members.includes(friendId);
+  if (alreadyMember) {return next(new AppError('User is already a member of the group', 400));}
+  group.members.push(friendId);
+  await group.save();
+  res.status(200).json({
+    status: 'success',
+    message: 'Friend added to group',
+    data: {group}
+  });
+})
+
+ exports.removeMember= catchAsync(async(req,res,next)=>{
+    const groupId= req.params.id;
+    const memberId=req.body.memberId;
+     if(!memberId) {return next(new AppError('Please provide a member ID', 400));}
+    const group = await Group.findById(groupId);
+   if (!group) return next(new AppError('Group not found', 404));
+   if(!group.createdBy.equals(req.user.id)) {
+    return next(new AppError('Only the group creator can remove members', 403));} 
+  const isMember = group.members.includes(memberId);
+    if (!isMember) {return next(new AppError('User is not a member of the group', 400));}
+    group.members = group.members.filter(member => member.toString() !== memberId);
+    await group.save();
+    res.status(200).json({
+        status: 'success',
+        message: 'Member removed from group',
+        data: {group}
+    });
+ });
+exports.getGroupMembers = catchAsync(async (req, res, next) => {
+    const group=await Group.findById(req.params.id).populate('members','name email');
+    if (!group) return next(new AppError('Group not found', 404));
+    res.status(200).json({
+        status: 'success',
+        data: {
+            members: group.members
+        }
+    });
+})
+exports.getMyGroups = catchAsync(async (req, res, next) => {
+    const groups = await Group.find({ members: req.user.id }).populate('members', 'name email');
+    if (!groups || groups.length === 0){
+        return next(new AppError('No groups found for this user', 404));
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            groups
+        }
+    });
+});
+
---
+++ backened/controllers/userController.js
@@ -0,0 +1,57 @@
+const User = require('./../modals/userschema');
+const catchAsync = require('./../utils/catchAsynch');
+const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
+const APIFeatures = require('./../utils/apiFeatures');
+const filterObj =(obj, ...allowedFields)=>{
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+exports.getme = (User,options={select: '-__v -password -passwordConfirm -active'}, popOptions) =>
+  catchAsync(async (req, res, next) => {
+   let query=Model.findById(req.user.id);
+    if (options.populate) {
+      query=query.populate(options.populate);
+    }
+    if (options.select) {
+      query=query.select(options.select);
+   }
+    const features=new APIFeatures(query, req.query).search();
+    const doc=await features.query;
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+exports.updateMe =catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates. Please use /updateMyPassword.',400));
+  }
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id,filteredBody,{
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id,{active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+
+
+
